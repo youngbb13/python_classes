9e32c191fe3ad8114cd557bd6ed23c40a1288af2
youngbb13 <153384410+youngbb13@users.noreply.github.com>
2026-01-08 15:05:12 +0100

    classes

---
+++ classes5.py
@@ -0,0 +1,25 @@
+'''Гарний приклад успадкування та поліморфізму'''
+
+class Shape:
+    def area(self):
+        pass
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * self.radius ** 2
+
+class Square(Shape):
+    def __init__(self, side):
+        self.side = side
+
+    def area(self):
+        return self.side ** 2
+
+circle = Circle(5)
+square = Square(5)
+
+print(circle.area())
+print(square.area())
